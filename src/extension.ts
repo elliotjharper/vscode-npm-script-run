e8fa38419bdea799990ca4e9657ee8fea540122b
elliotjharper <50680194+elliotjharper@users.noreply.github.com>
2022-09-21 19:11:15 +0100

    Published v 1.0 and 1.0.1

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,33 @@ import * as vscode from 'vscode';
 import { readNpmScripts } from './read-npm-scripts';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('elltg-hello-world.helloWorld', async () => {
-        const namedNpmScripts = await readNpmScripts();
+    let disposable = vscode.commands.registerCommand(
+        'elltg-npm-script-run.runNpmScript',
+        async () => {
+            const namedNpmScripts = await readNpmScripts();
 
-        const selectedNpmScript = await vscode.window.showQuickPick(namedNpmScripts);
+            const selectedNpmScript = await vscode.window.showQuickPick(namedNpmScripts);
 
-        if (!selectedNpmScript) {
-            vscode.window.showInformationMessage('You did not select an npm script. Exiting...');
-            return;
+            if (!selectedNpmScript) {
+                vscode.window.showInformationMessage(
+                    'You did not select an npm script. Exiting...'
+                );
+                return;
+            }
+
+            vscode.window.showInformationMessage(
+                `Running selected npm script: [${selectedNpmScript}]`
+            );
+
+            const activeTerminal = vscode.window.activeTerminal;
+            if (activeTerminal) {
+                activeTerminal.show();
+                activeTerminal.sendText(`npm run ${selectedNpmScript}`);
+            } else {
+                vscode.window.showInformationMessage('No active terminal. Exiting...');
+            }
         }
-
-        vscode.window.showInformationMessage(`Running selected npm script: [${selectedNpmScript}]`);
-
-        const activeTerminal = vscode.window.activeTerminal;
-        if (activeTerminal) {
-            activeTerminal.show();
-            activeTerminal.sendText(`npm run ${selectedNpmScript}`);
-        } else {
-            vscode.window.showInformationMessage('No active terminal. Exiting...');
-        }
-    });
+    );
 
     context.subscriptions.push(disposable);
 }
