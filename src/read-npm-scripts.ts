9fb3c2d7fe3464e787a80f3d8062298b056bda37
RJiazhen <jiazhen.ruan@outlook.com>
2024-09-01 16:25:48 +0800

    refactor: delete old function

--- src/read-npm-scripts.ts
+++ src/read-npm-scripts.ts
@@ -62,32 +62,3 @@ export async function getNpmScriptFromPackageJson(packageJsonPath: string): Prom
 
     return packageScriptList;
 }
-
-async function readPackageJson(): Promise<IPackageFileWithScripts> {
-    const workspaceRootUri = vscode.workspace.workspaceFolders?.[0]?.uri;
-    if (!workspaceRootUri) {
-        throw new Error('folder needed!');
-    }
-
-    const workspacePackageJsonUri = vscode.Uri.joinPath(workspaceRootUri, './package.json');
-
-    const packageJsonTextDocument = await vscode.workspace.openTextDocument(
-        workspacePackageJsonUri
-    );
-
-    const packageJsonTextContents = packageJsonTextDocument.getText();
-
-    return JSON.parse(packageJsonTextContents);
-}
-
-export async function readNpmScripts(): Promise<string[]> {
-    try {
-        const packageJson = await readPackageJson();
-
-        const packageScripts = Object.keys(packageJson.scripts).sort();
-
-        return packageScripts;
-    } catch (err) {
-        return [`No package.json file found! Error: ${err}`];
-    }
-}
